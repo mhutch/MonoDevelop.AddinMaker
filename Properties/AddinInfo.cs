d9ca7753158242dcb4a22f5cb87eccf770ac1629
Michael Hutchinson <m.j.hutchinson@gmail.com>
2014-05-20 12:44:09 -0400

    Update to 5.0

---
+++ Properties/AddinInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"AddinMaker",
+	Namespace = "MonoDevelop",
+	Version = "1.0.1",
+	Url = "http://github.com/mhutch/MonoDevelop.AddinMaker"
+)]
+
+[assembly:AddinName ("Addin Maker")]
+[assembly:AddinCategory ("Addin Development")]
+[assembly:AddinDescription ("Makes it easy to create and edit addins")]
+[assembly:AddinAuthor ("Michael Hutchinson")]
+
+[assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
---
+++ Templates/Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"${ProjectName}", 
+	Namespace = "${ProjectName}",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("${ProjectName}")]
+[assembly:AddinCategory ("${ProjectName}")]
+[assembly:AddinDescription ("${ProjectName}")]
+[assembly:AddinAuthor ("${AuthorName}")]
+
+[assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
