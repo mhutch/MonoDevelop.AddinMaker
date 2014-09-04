2958c380bab86aafabf26d78a2ed6d534ed49311
Michael Hutchinson <m.j.hutchinson@gmail.com>
2014-09-03 20:07:40 -0400

    Improve MD location lookup in build tasks

--- MonoDevelop.Addins.Tasks/GetDefaultMonoDevelopLocations.cs
+++ MonoDevelop.Addins.Tasks/GetDefaultMonoDevelopLocations.cs
@@ -8,29 +8,43 @@ namespace MonoDevelop.Addins.Tasks
 	public class GetDefaultMonoDevelopLocations : Task
 	{
 		[Required]
-		public string ProfileID { get; set; }
+		public string ProfileVersion { get; set; }
+
+		public string ProfileName { get; set; }
 
 		public string ProfilePath { get; set; }
 
 		// Based on logic in MonoDevelop.Core.UserProfile
 		public override bool Execute ()
 		{
+			string profileID = ProfileName;
+
+			if (string.IsNullOrEmpty (profileID)) {
+				if (Platform.IsWindows || Platform.IsMac) {
+					profileID = "XamarinStudio";
+				} else {
+					profileID = "MonoDevelop";
+				}
+			}
+
+			profileID = profileID + "-" + ProfileVersion;
+
 			if (!string.IsNullOrEmpty (ProfilePath)) {
-				ConfigDir = Path.Combine (ProfilePath, ProfileID, "Config");
-				AddinsDir = Path.Combine (ProfilePath, ProfileID, "LocalInstall", "Addins");
-				DatabaseDir = Path.Combine (ProfilePath, ProfileID, "Cache");
+				ConfigDir = Path.Combine (ProfilePath, profileID, "Config");
+				AddinsDir = Path.Combine (ProfilePath, profileID, "LocalInstall", "Addins");
+				DatabaseDir = Path.Combine (ProfilePath, profileID, "Cache");
 			} else if (Platform.IsWindows) {
 				string local = Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData);
 				string roaming = Environment.GetFolderPath (Environment.SpecialFolder.ApplicationData);
-				ConfigDir = Path.Combine (roaming, ProfileID, "Config");
-				AddinsDir = Path.Combine (local, ProfileID, "LocalInstall", "Addins");
-				DatabaseDir = Path.Combine (local, ProfileID, "Cache");
+				ConfigDir = Path.Combine (roaming, profileID, "Config");
+				AddinsDir = Path.Combine (local, profileID, "LocalInstall", "Addins");
+				DatabaseDir = Path.Combine (local, profileID, "Cache");
 			} else if (Platform.IsMac) {
 				string home = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
 				string library = Path.Combine (home, "Library");
-				ConfigDir = Path.Combine (library, "Preferences", ProfileID);
-				AddinsDir = Path.Combine (library, "Application Support", ProfileID, "LocalInstall", "Addins");
-				DatabaseDir = Path.Combine (library, "Caches", ProfileID);
+				ConfigDir = Path.Combine (library, "Preferences", profileID);
+				AddinsDir = Path.Combine (library, "Application Support", profileID, "LocalInstall", "Addins");
+				DatabaseDir = Path.Combine (library, "Caches", profileID);
 			} else {
 				string home = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
 				string xdgDataHome = Environment.GetEnvironmentVariable ("XDG_DATA_HOME");
@@ -42,9 +56,9 @@ namespace MonoDevelop.Addins.Tasks
 				string xdgCacheHome = Environment.GetEnvironmentVariable ("XDG_CACHE_HOME");
 				if (string.IsNullOrEmpty (xdgCacheHome))
 					xdgCacheHome = Path.Combine (home, ".cache");
-				ConfigDir = Path.Combine (xdgConfigHome, ProfileID);
-				AddinsDir = Path.Combine (xdgDataHome, ProfileID, "LocalInstall");
-				DatabaseDir = Path.Combine (xdgCacheHome, ProfileID);
+				ConfigDir = Path.Combine (xdgConfigHome, profileID);
+				AddinsDir = Path.Combine (xdgDataHome, profileID, "LocalInstall");
+				DatabaseDir = Path.Combine (xdgCacheHome, profileID);
 			}
 
 			if (Platform.IsWindows) {
