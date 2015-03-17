a3b56147c970b01d7f3c8b8bb345fcf8b369c585
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-03-16 23:10:41 -0400

    Make publish script work on Windows

--- MonoDevelop.Addins.Tasks/publish.sh
+++ MonoDevelop.Addins.Tasks/publish.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# download nuget on Windows
+# it should always be present on Mono
+nuget > /dev/null || curl https://api.nuget.org/downloads/nuget.exe -o nuget.exe
+
 ## CAUTION HACK
 
 rm *.nupkg
