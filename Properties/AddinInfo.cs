a4492ed5f2ffb93c36d45703198bd08d7aa30b8b
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-03-23 22:53:58 -0400

    Release 1.2.1

--- Properties/AddinInfo.cs
+++ Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"AddinMaker",
 	Namespace = "MonoDevelop",
-	Version = "1.2.0",
+	Version = "1.2.1",
 	Url = "http://github.com/mhutch/MonoDevelop.AddinMaker"
 )]
 
