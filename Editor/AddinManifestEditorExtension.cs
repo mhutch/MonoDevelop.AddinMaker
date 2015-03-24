2cd64a3dcf93921cdebd3ebc745dd99488d3b2a2
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-03-23 22:53:38 -0400

    Add some more attributes to manifest

--- Editor/AddinManifestEditorExtension.cs
+++ Editor/AddinManifestEditorExtension.cs
@@ -24,7 +24,24 @@ namespace MonoDevelop.AddinMaker.Editor
 			};
 
 			return new SchemaElement (null, null, new[] {
-				new SchemaElement ("Addin", "Root element for add-in and add-in root descriptions", addinContents),
+				new SchemaElement (
+					"Addin",
+					"Root element for add-in and add-in root descriptions",
+					addinContents,
+					new[] {
+						new SchemaAttribute ("id", "The identifier of the add-in. It is mandatory for add-in roots and for add-ins that can be extended, optional for other add-ins."),
+						new SchemaAttribute ("namespace", "Namespace of the add-in. The full ID of an add-in is composed by 'namespace.name'."),
+						new SchemaAttribute ("version", "The version of the add-in. It is mandatory for add-in roots and for add-ins that can be extended."),
+						new SchemaAttribute ("compatVersion", "Version of the add-in with which this add-in is backwards compatible (optional)."),
+						new SchemaAttribute ("name", "Display name of the add-in."),
+						new SchemaAttribute ("description", "Description of the add-in."),
+						new SchemaAttribute ("author", "Author of the add-in."),
+						new SchemaAttribute ("url", "Url of a web page with more information about the add-in."),
+						new SchemaAttribute ("defaultEnabled", "When set to 'false', the add-in won't be enabled until it is explicitly enabled by the user. The default is 'true'."),
+						//TODO: enable this if we ever support arbitraty addins
+						//new SchemaAttribute ("isroot", "Must be true if this manifest belongs to an add-in root.")
+					}
+				),
 				new SchemaElement ("ExtensionModel", "Root element for add-in and add-in root descriptions", addinContents)
 			});
 		}
