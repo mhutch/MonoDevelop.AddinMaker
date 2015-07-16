23e365a97439a17375f205f5bedfa997307ed27a
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-07-15 22:14:05 -0400

    Use MSBuild 14 from build script

--- Install.bat
+++ Install.bat
@@ -1,9 +1,9 @@
 pushd %~dp0
 
-"%PROGRAMFILES(X86)%\MSBuild\12.0\Bin\MSBuild.exe" /t:Build /p:Configuration=Debug MonoDevelop.AddinMaker.sln
+"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\MSBuild.exe" /t:Build /p:Configuration=Debug MonoDevelop.AddinMaker.sln
 if not %ERRORLEVEL% == 0 goto Error
 
-"%PROGRAMFILES(X86)%\MSBuild\12.0\Bin\MSBuild.exe" /t:InstallAddin /p:Configuration=Debug MonoDevelop.AddinMaker.csproj
+"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\MSBuild.exe" /t:InstallAddin /p:Configuration=Debug MonoDevelop.AddinMaker.csproj
 if not %ERRORLEVEL% == 0 goto Error
 
 goto Exit
