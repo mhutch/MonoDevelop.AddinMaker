df614d75a385ee0b29713db6b7303f4980572a1c
mhutch <m.j.hutchinson@gmail.com>
2016-01-22 16:47:05 -0500

    Bump version

--- Properties/AddinInfo.cs
+++ Properties/AddinInfo.cs
@@ -5,11 +5,11 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"AddinMaker",
 	Namespace = "MonoDevelop",
-	Version = "1.2.1",
+	Version = "1.3.0",
 	Url = "http://github.com/mhutch/MonoDevelop.AddinMaker"
 )]
 
 [assembly:AddinName ("Addin Maker")]
 [assembly:AddinCategory ("Addin Development")]
 [assembly:AddinDescription ("Makes it easy to create and edit addins")]
-[assembly:AddinAuthor ("Michael Hutchinson")]
+[assembly:AddinAuthor ("mhutch")]
