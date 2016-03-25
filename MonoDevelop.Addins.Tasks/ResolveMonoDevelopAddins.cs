65ee1032e9098ec744f69a256388791e38c69b6b
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2016-03-25 19:29:15 -0400

    Dynamically resolve core references
    So I don't keep having to update them when XS adds assemblies...

--- MonoDevelop.Addins.Tasks/ResolveMonoDevelopAddins.cs
+++ MonoDevelop.Addins.Tasks/ResolveMonoDevelopAddins.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using Mono.Addins;
+using System.Reflection;
 
 namespace MonoDevelop.Addins.Tasks
 {
@@ -32,6 +33,11 @@ namespace MonoDevelop.Addins.Tasks
 			bool success = true;
 
 			var resolvedAddins = new List<ITaskItem> ();
+
+			//core addins don't declare imports for assemblies in bin, and these can change between releases
+			//so we inspect core addin assemblies' references and transitively add them all
+			var coreReferences = new Dictionary<string,string> ();
+			string coreRoot = Path.GetFullPath (Path.Combine (BinDir, "..")) + Path.DirectorySeparatorChar;
 
 			foreach (var addinName in AddinReferences) {
 				var addin = Registry.GetAddin (addinName.ItemSpec);
@@ -66,6 +72,11 @@ namespace MonoDevelop.Addins.Tasks
 
 					assemblies.Add (addinAssemblyPath);
 					Log.LogMessage (MessageImportance.Low, "Found addin assembly at path '{0}'", addinAssemblyPath);
+
+					//for assemblies that ship in MD, search their dependencies and add references
+					if (addinAssemblyPath.StartsWith (coreRoot, StringComparison.OrdinalIgnoreCase)) {
+						CollectCoreReferences (addinAssemblyPath, coreReferences);
+					}
 				}
 			}
 
@@ -75,7 +86,7 @@ namespace MonoDevelop.Addins.Tasks
 				VersionDefines = "MD_" + core.Description.CompatVersion.Replace ('.', '_');
 			}
 
-			AssemblyReferences = assemblies.Select (a => {
+			AssemblyReferences = (assemblies.Concat (coreReferences.Values.Where (v => v != null))).Select (a => {
 				var item = new TaskItem (Path.GetFileNameWithoutExtension (a));
 				item.SetMetadata ("Private", "False");
 				item.SetMetadata ("HintPath", a);
@@ -85,6 +96,25 @@ namespace MonoDevelop.Addins.Tasks
 			ResolvedAddins = resolvedAddins.ToArray ();
 
 			return success;
+		}
+
+		void CollectCoreReferences (string addinAssembly, Dictionary<string,string> coreReferences)
+		{
+			var asm = Assembly.ReflectionOnlyLoadFrom (addinAssembly);
+			var referenced = asm.GetReferencedAssemblies ();
+			foreach (var r in referenced) {
+				if (coreReferences.ContainsKey (r.Name))
+					continue;
+				var p = Path.Combine (BinDir, r.Name + ".dll");
+				if (File.Exists (p)) {
+					var fullPath = Path.GetFullPath (p);
+					coreReferences.Add (r.Name, fullPath);
+					Log.LogMessage (MessageImportance.Low, "Added transitive reference '{0}'", r);
+					CollectCoreReferences (fullPath, coreReferences);
+				} else {
+					coreReferences.Add (r.Name, null);
+				}
+			}
 		}
 	}
 }
