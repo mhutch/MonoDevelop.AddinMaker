ba3d310d8579f7c4066d0f4541ef6d9c81537a32
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2016-06-22 01:23:08 -0400

    Unit tests work!

---
+++ MonoDevelop.AddinMaker.Tests/Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+namespace MonoDevelop.AddinMaker.Tests
+{
+	[TestFixture]
+	public class GlobalGroperties
+	{
+		[Test]
+		public void GetProperties ()
+		{
+			var provider = new AddinMSBuildGlobalPropertyProvider ();
+			var props = provider.GetGlobalProperties ();
+
+			Assert.AreEqual (3, props.Count);
+		}
+	}
+}
