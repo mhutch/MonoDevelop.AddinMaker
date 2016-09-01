499dc3934a8b1961510d2c426dccb360f8deb9a7
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2016-09-01 13:34:32 -0400

    Parameterize GuiUnit package version

--- MonoDevelop.Addins.TestRunner/packguiunit.sh
+++ MonoDevelop.Addins.TestRunner/packguiunit.sh
@@ -1,6 +1,7 @@
 #bin/sh
 
 REVISION=2670780396856f043ab5cea9ab856641f56de5ae
+VERSION=0.1.1
 
 pushd ../../guiunit
 git reset --hard $REVISION || exit 1
@@ -11,5 +12,5 @@ rm MonoDevelop.Addins.GuiUnit*.nupkg
 mkdir guiunit
 cp ../../guiunit/bin/net_4_5/GuiUnit.exe guiunit
 cp ../../guiunit/LICENSE.txt guiunit
-mono nuget.exe pack MonoDevelop.Addins.GuiUnit.nuspec
+mono nuget.exe pack MonoDevelop.Addins.GuiUnit.nuspec -Properties version=$VERSION
 rm -r guiunit
