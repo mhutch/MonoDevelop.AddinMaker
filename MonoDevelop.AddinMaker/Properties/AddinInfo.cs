8b04a24e4934d2b3996b001f4b499da4cb630bef
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-01-11 16:50:02 -0500

    Version 1.3.4

--- MonoDevelop.AddinMaker/Properties/AddinInfo.cs
+++ MonoDevelop.AddinMaker/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"AddinMaker",
 	Namespace = "MonoDevelop",
-	Version = "1.3.3",
+	Version = "1.3.4",
 	Url = "http://github.com/mhutch/MonoDevelop.AddinMaker"
 )]
 
