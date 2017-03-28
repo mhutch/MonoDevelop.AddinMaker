7cd3e0bf78fa15e3328f6d140df6df6a375e4728
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-03-28 12:59:48 -0400

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,14 +1,35 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+
 pushd %~dp0
 
-"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\MSBuild.exe" /p:InstallAddin=True %*
-if not %ERRORLEVEL% == 0 goto Error
+rem try to find MSBuild in VS2017
+rem the "correct" way is to use a COM API. not easy here.
 
-goto Exit
+FOR %%E in (Enterprise, Professional, Community) DO (
+	set "MSBUILD_EXE=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%%E\MSBuild\15.0\Bin\MSBuild.exe"
+	if exist "!MSBUILD_EXE!" goto :build
+)
 
-:Error
-echo ERROR
-pause
-goto Exit
+echo Could not find MSBuild 15
+popd
+exit /b 1
 
-:Exit
+:build
+
+set "CONFIG=Release"
+set "PLATFORM=Any CPU"
+
+"%MSBUILD_EXE%" /target:Restore %* || goto :error
+"%MSBUILD_EXE%" /m "/p:Configuration=%CONFIG%" "/p:Platform=%PLATFORM%" /p:InstallAddin=True %* || goto :error
+
+goto :exit
+
+:error
+
+for %%x in (%CMDCMDLINE%) do if /i "%%~x" == "/c" pause
+popd
+exit /b %ERRORLEVEL%
+
+:exit
 popd
