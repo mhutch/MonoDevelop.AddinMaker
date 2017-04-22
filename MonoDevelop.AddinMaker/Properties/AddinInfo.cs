8253856ecb02c7fe3fc987b6148f1bc0ce2cf545
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-04-21 20:30:51 -0400

    Bump addin and templates version

--- MonoDevelop.AddinMaker/Properties/AddinInfo.cs
+++ MonoDevelop.AddinMaker/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"AddinMaker",
 	Namespace = "MonoDevelop",
-	Version = "1.3.5",
+	Version = "1.3.6",
 	Url = "http://github.com/mhutch/MonoDevelop.AddinMaker"
 )]
 
