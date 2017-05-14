6c39cb1b9ba2bf8d4eccf6e233dbb0544ea5dbec
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-05-14 15:17:22 -0700

    Bump versions
    Need to update Mono.Addins to fix issue where it
    referenced the wrong SharpZipLib version.

--- MonoDevelop.AddinMaker/Properties/AddinInfo.cs
+++ MonoDevelop.AddinMaker/Properties/AddinInfo.cs
@@ -5,11 +5,11 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"AddinMaker",
 	Namespace = "MonoDevelop",
-	Version = "1.3.6",
+	Version = "1.3.7",
 	Url = "http://github.com/mhutch/MonoDevelop.AddinMaker"
 )]
 
-[assembly:AddinName ("Addin Maker")]
+[assembly:AddinName ("AddinMaker")]
 [assembly:AddinCategory ("Extension Development")]
 [assembly:AddinDescription ("Makes it easy to create and edit IDE extensions")]
 [assembly:AddinAuthor ("Mikayla Hutchinson")]
