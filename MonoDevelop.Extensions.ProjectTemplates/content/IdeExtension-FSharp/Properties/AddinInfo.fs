de423df492ec0ec82d56664f0901691491b5a264
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-09-28 18:02:34 -0400

    Switch project templates to new engine

---
+++ MonoDevelop.Extensions.ProjectTemplates/content/IdeExtension-FSharp/Properties/AddinInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace IdeExtension
+
+open System
+open Mono.Addins
+open Mono.Addins.Description
+
+[<assembly:Addin("IdeExtension", Namespace = "IdeExtension", Version = "1.0")>]
+[<assembly:AddinName("IdeExtension")>]
+[<assembly:AddinCategory("IDE extensions")>]
+[<assembly:AddinDescription("IdeExtension")>]
+[<assembly:AddinAuthor("Mikayla Hutchinson")>]
+()
