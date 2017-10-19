5eb88591e43eafde7054928f1ad95c2586eb78f8
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-10-19 18:20:15 -0400

    Fix broken template & release 1.4,1

--- MonoDevelop.AddinMaker/Properties/AddinInfo.cs
+++ MonoDevelop.AddinMaker/Properties/AddinInfo.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using Mono.Addins;
+﻿using Mono.Addins;
 using Mono.Addins.Description;
 
-[assembly:Addin (
+[assembly: Addin (
 	"AddinMaker",
 	Namespace = "MonoDevelop",
-	Version = "1.3.7",
+	Version = "1.4.1",
 	Url = "http://github.com/mhutch/MonoDevelop.AddinMaker"
 )]
 
-[assembly:AddinName ("AddinMaker")]
-[assembly:AddinCategory ("Extension Development")]
-[assembly:AddinDescription ("Makes it easy to create and edit IDE extensions")]
-[assembly:AddinAuthor ("Mikayla Hutchinson")]
+[assembly: AddinName ("AddinMaker")]
+[assembly: AddinCategory ("Extension Development")]
+[assembly: AddinDescription ("Makes it easy to create and edit IDE extensions")]
+[assembly: AddinAuthor ("Mikayla Hutchinson")]
