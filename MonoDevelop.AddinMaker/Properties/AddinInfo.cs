0ce8213aeea5fce87de43d9811166b2f69b85e9d
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2018-08-08 14:27:22 -0400

    Bump to 1.4.3

--- MonoDevelop.AddinMaker/Properties/AddinInfo.cs
+++ MonoDevelop.AddinMaker/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin (
 	"AddinMaker",
 	Namespace = "MonoDevelop",
-	Version = "1.4.2",
+	Version = "1.4.3",
 	Url = "http://github.com/mhutch/MonoDevelop.AddinMaker"
 )]
 
